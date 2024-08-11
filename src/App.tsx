920ff9c44c51ccad5c7acd5d4bf4e76e43d0a886
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2024-08-11 10:40:53 +0330

    [#1-UPDATE] chat engine setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,14 @@ import "./App.css"
 
 const App = () => {
     return (
-        <div className="App">
-            <ChatEngine/>
+        <div className="">
+            <ChatEngine
+                height='100vh'
+                projectID='a553d44b-6350-4fc9-904b-de0b05e35522'
+                userName='mamadaghaei'
+                userSecret='1234'
+            />
         </div>
     )
 }
+export default App
