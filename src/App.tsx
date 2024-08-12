b006a11a2ccc21d70232e5226ff99898d553998a
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2024-08-12 20:37:41 +0330

    [#1-ADD] chat feed component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { ChatEngine} from 'react-chat-engine'
 import "./App.css"
+import ChatFeed from "./Components/ChatFeed.tsx";
 
 const App = () => {
     return (
@@ -9,6 +10,7 @@ const App = () => {
                 projectID='a553d44b-6350-4fc9-904b-de0b05e35522'
                 userName='mamadaghaei'
                 userSecret='1234'
+                renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
             />
         </div>
     )
