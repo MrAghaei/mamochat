727c0e60f5a0b14c4817e1dbf97ad926d05c191f
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2024-08-15 10:11:56 +0330

    [#2-Implement] TheirMessage component

--- src/Components/MyMessage.tsx
+++ src/Components/MyMessage.tsx
@@ -4,10 +4,12 @@ const MyMessage = ({message}) => {
             <img src={message?.attachment[0]} alt="message-attachment" className="message-image" style={{float: 'right'}}/>
         )
     }
-
+    console.log(message.text);
     return (
         <div className='message' style={{float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#3B2A50'}}>
             {message.text}
+
+
         </div>
     )
 }
