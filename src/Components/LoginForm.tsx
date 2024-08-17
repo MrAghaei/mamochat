dc17964da74ee34f2c4d76f93825b556f2ded566
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2024-08-17 21:35:35 +0330

    [#3-Implement] login localstorage

--- src/Components/LoginForm.tsx
+++ src/Components/LoginForm.tsx
@@ -4,18 +4,21 @@ import axios from 'axios'
 const LoginForm = ()=>{
     const [userName, setUserName] = useState('');
     const [password, setPassword] = useState('')
+    const [error, setError] = useState('')
 
     const handleSubmit = async (e)=>{
         e.preventDefault()
-        const authObject: any  = {'Porject-ID':'a553d44b-6350-4fc9-904b-de0b05e35522', 'User-Name': userName, 'User-Secret': password}
+        const authObject  = {'Project-ID':'a553d44b-6350-4fc9-904b-de0b05e35522', 'User-Name': userName, 'User-Secret': password}
         try {
             await axios.get('https://api.chatengine.io/chats', {headers: authObject})
 
             localStorage.setItem('username', userName)
             localStorage.setItem('password', password)
+            window.location.reload()
 
         }catch (error){
-
+            setError('Wrong Credentials')
+            console.log(error);
         }
 
     }
@@ -31,12 +34,12 @@ const LoginForm = ()=>{
                     <input type='password' className='input' value={password} onChange={(e) => {
                         setPassword(e.target.value)
                     }} placeholder='Password' required/>
-                    <div>
+                    <div align='center'>
                         <button type='submit' className='button'>
                             <span>Start Chatting</span>
                         </button>
                     </div>
-
+                    <h2 className='error'>{error}</h2>
                 </form>
             </div>
         </div>
